ed62bdd222967c89c28215d3644434850d70246c
KurtPfeifle <kurt.pfeifle@gmail.com>
2018-01-02 13:49:58 +0100

    Amend output of 'appimagekit-toolbox-usage.sh'

--- appimage/appimagekit-toolbox-usage.sh
+++ appimage/appimagekit-toolbox-usage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo ""
-echo " Usage: $ARGV0 <sub-command>"
+echo " Usage:   $ARGV0  <sub-command>"
 echo ""
-echo "   Available sub-commands are:"
+echo "   Available sub-commands:"
 echo ""
 echo "      AppImageUpdate     (GUI tool)"
 echo "      AppRun             (not meant to be run by users)"
@@ -12,7 +12,7 @@ echo "      appimagetool       (tool to inspect and create AppImages from AppDir
 echo "      appimageupdatetool (tool to search for and apply binary delta updates to existing AppImages)"
 echo "      linuxdeployqt      (tool to optimize AppDirs by including all libraries required by its binaries)"
 echo "      runtime            (not meant to be run by users)"
-echo "      zsync2             (tool to...)"
-echo "      zsyncmake2         (tool to..)"
+echo "      zsync2             (tool to easily update files to newer versions via highly efficient 'delta' updates)"
+echo "      zsyncmake2         (tool to create 'foo.zsync' file for existing 'foo' file for later zsync2-update)"
 echo ""
 
