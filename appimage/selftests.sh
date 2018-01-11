d2b10513c551853405bd9ec290ec850c1deb9b12
KurtPfeifle <kurt.pfeifle@gmail.com>
2018-01-11 16:26:48 +0100

    Change order of selftests.sh

--- appimage/selftests.sh
+++ appimage/selftests.sh
@@ -7,12 +7,12 @@ if [ x$aiktb_debug == xyes ] ; then
 fi
 
 (
+  bash usr/share/tests/check_lists          ;
+  bash usr/share/tests/check_help           ;
+  bash usr/share/tests/check_appimagehelp   ;
+  bash usr/share/tests/check_updatedescribe ;
   bash usr/share/tests/check_updateinfo     ;
   bash usr/share/tests/check_offsets        ;
   bash usr/share/tests/check_versions       ;
-  bash usr/share/tests/check_updatedescribe ;
-  bash usr/share/tests/check_appimagehelp   ;
-  bash usr/share/tests/check_help           ;
-  bash usr/share/tests/check_lists          ;
 ) | ./usr/bin/less
 
