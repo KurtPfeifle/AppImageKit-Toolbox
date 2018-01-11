b5a5cf585792e1fd676ac3fe9034036ecb77b8f9
KurtPfeifle <kurt.pfeifle@gmail.com>
2018-01-11 13:33:35 +0100

    Add '--appimage-offset' to selftests.sh

--- appimage/selftests.sh
+++ appimage/selftests.sh
@@ -4,10 +4,11 @@
 #pwd
 
 ( bash usr/share/tests/check_updateinfo     ;
-  bash usr/share/tests/check_offset         ;
+  bash usr/share/tests/check_offsets        ;
   bash usr/share/tests/check_versions       ;
   bash usr/share/tests/check_updatedescribe ;
   bash usr/share/tests/check_appimagehelp   ;
   bash usr/share/tests/check_help           ;
-  bash usr/share/tests/check_lists ) | less
+  bash usr/share/tests/check_lists          ;
+  which less ) | ./usr/bin/less
 
