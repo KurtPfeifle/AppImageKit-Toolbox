f57d47bf28bd86f44e3dfbb13b13678918e7bdb8
KurtPfeifle <kurt.pfeifle@gmail.com>
2018-01-11 13:38:26 +0100

    Env var "aiktb_debug" (yes) enables selftest.sh debugging

--- appimage/selftests.sh
+++ appimage/selftests.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-#set -x
-#pwd
 
-( bash usr/share/tests/check_updateinfo     ;
+if [ x$aiktb_debug == xyes ] ; then
+    set -x
+    pwd
+fi
+
+(
+  bash usr/share/tests/check_updateinfo     ;
   bash usr/share/tests/check_offsets        ;
   bash usr/share/tests/check_versions       ;
   bash usr/share/tests/check_updatedescribe ;
   bash usr/share/tests/check_appimagehelp   ;
   bash usr/share/tests/check_help           ;
   bash usr/share/tests/check_lists          ;
-  which less ) | ./usr/bin/less
+) | ./usr/bin/less
 
