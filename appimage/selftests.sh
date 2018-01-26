a23f3bded70965ac04e3b92fb3f8fb4e64c34848
KurtPfeifle <kurt.pfeifle@gmail.com>
2018-01-26 23:31:43 +0100

    Fix 'selftests.sh' for (meta-)AppImage appimage-toolkit itself

--- appimage/selftests.sh
+++ appimage/selftests.sh
@@ -10,10 +10,10 @@ fi
   bash usr/share/tests/check_lists            ;
   bash usr/share/tests/check_help             ;
   bash usr/share/tests/check_appimagehelp     ;
-  bash usr/share/tests/check_updatedescribe   ;
-  bash usr/share/tests/check_updateinfo       ;
   bash usr/share/tests/check_offsets          ;
   bash usr/share/tests/check_versions         ;
   bash usr/share/tests/check_appimageversions ;
+  bash usr/share/tests/check_updateinfo       ;
+  bash usr/share/tests/check_updatedescribe   ;
 ) | ./usr/bin/less
 
