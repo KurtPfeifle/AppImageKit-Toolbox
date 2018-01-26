d366bbdd84ab195943bb5e392c14a4ad94134ec7
KurtPfeifle <kurt.pfeifle@gmail.com>
2018-01-26 23:46:19 +0100

    Reshuffle order of selftests

--- appimage/selftests.sh
+++ appimage/selftests.sh
@@ -7,13 +7,13 @@ if [ x$aiktb_debug == xyes ] ; then
 fi
 
 (
-  bash usr/share/tests/check_lists            ;
-  bash usr/share/tests/check_help             ;
-  bash usr/share/tests/check_appimagehelp     ;
-  bash usr/share/tests/check_offsets          ;
   bash usr/share/tests/check_versions         ;
+  bash usr/share/tests/check_offsets          ;
   bash usr/share/tests/check_appimageversions ;
   bash usr/share/tests/check_updateinfo       ;
   bash usr/share/tests/check_updatedescribe   ;
+  bash usr/share/tests/check_help             ;
+  bash usr/share/tests/check_appimagehelp     ;
+  bash usr/share/tests/check_lists            ;
 ) | ./usr/bin/less
 
